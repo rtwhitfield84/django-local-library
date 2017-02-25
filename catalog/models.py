7b8eb2f2e93c7712415c723d0898ee13dda1702c
Rico <richardthomaswhitfield@gmail.com>
2017-02-25 14:26:38 -0600

    updated admin

--- catalog/models.py
+++ catalog/models.py
@@ -31,6 +31,13 @@ class Book(models.Model):
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    def display_genre(self):
+            """
+            Creates a string for the Genre. This is required to display genre in Admin.
+            """
+            return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
     
     def __str__(self):
         """
